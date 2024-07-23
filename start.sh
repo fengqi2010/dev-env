6faea498398d5e37b625218cfebb458d938033a5
breeze <fengqi0508@gmail.com>
2024-07-23 10:23:25 +0000

    init

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
+chmod +x ./kind
+#
+./kind create cluster -n k8s --config kind-config.yaml
